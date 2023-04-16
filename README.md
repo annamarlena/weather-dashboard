237f653de56255bb330c11f7ec702a7545f80ef6
Anna Marlena <annamarlena310@gmail.com>
2023-04-15 21:36:08 -0500

    Did some serious work on the JavaScript today and updated the README

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 ## Objective
 
-The objective of this assignment was to create a weather dashboard with form inputs where a user can search weather forecasts for multiple cities. After searching a city, the dashboard should show current weather conditions as well as the 5-day forecast for that city. When the user searches a new city, the previous one is saved to the search history. When the user clicks on a city in the search history, that city's weather data is again displayed for the user.
+The objective was to create a weather dashboard with form inputs where a user can search weather forecasts for multiple cities. After searching a city, the dashboard should show current weather conditions as well as the 5-day forecast for that city. When the user searches a new city, the previous one is saved to the search history. When the user clicks on a city in the search history, that city's weather data is again displayed for the user.
 
-In addition,I was learning Bulma as a new techonlogy for a group project and decided to inmplement what I learned into the styling for this project also. (Bulma is a free open-source library that works with Flexbox to make application styling simple, efficient, and elegant.)
+In addition, I'm learning Bulma as a new techonlogy for a group project and decided to inmplement what I've learned into the styling for this project as well. (Bulma is a free open-source library that works with Flexbox to make application styling simple, efficient, and elegant.)
 
 ## What I Learned
 
-
+Mainly what I learned here is to balance priorities so I don't miss a deadline. I'll get this project done but not in time expected timeframe, mainly due to being overly focused on a different project.
 
 ## Link to Published Repo
 
+
 ## Screenshot
+
+![Here's a screenshot of my work so far](./assets/screenshot.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,65 @@
 // Declare the global variables
 let searchInput = $("#city-name")
 const searchBtn = $("#search-btn")
-
 const APIkey = "8e97a91e5813b6e17ff52b89e396e190"
-let cityName
-
-
 
 function searchCity() {
   // Grab city name entered by the user
-  var searchInputValue = searchInput.val();
+  let cityName = searchInput.val();
 
    // Return error if the input field is blank
-   if(!searchInputValue){
+   if(!cityName){
     alert("Please enter a city name.");
     return;
 }
 // Send the query
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey.val();
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
 
-fetch(queryURL)
+fetch(queryURL).then( function(response) {
+  return response.json()
+}).then( function(data) {
+    console.log(data)
+    var currentWeather = $("#current-weather")
+    var tempEL = $("<p>").text("Current temp: " + data.main.temp + "F").addClass("is-link")
+    currentWeather.append(tempEL)
 
-console.log(queryURL.val())
+
+    fiveDay(cityName)
+})
+console.log(queryURL)
 }
 
+function fiveDay(cityName) {
+  
+  var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey + "&units=imperial";
 
-// Add event listener for the search button
-searchBtn.on("click", function(e){
-  e.preventDefault();
-  searchCity()
+  fetch(queryURL).then( function(response) {
+    return response.json()
+  }).then( function(data) {
+      console.log(data)
+      const daysInForecast = data.list
+      console.log(queryURL)
+      /*
+      Each date object has a property called "dt", which is a Unix timestamp for the date and time 
+      of that object's data. The first one is 1681333200.
+      */ 
+     
+     // Create a new array to hold one day block per forecast day.
+     const newForecastArr = [] 
+     
+     // iterate over the 40 blocks, but we will do them 8 at a time, so that we get one per day.
+     for( let i=0; i<40; i=i+8 ){
+       newForecastArr.push( daysInForecast[i])
+      }
+      
+      // We now have a new array with one record for each day!
+      console.log(newForecastArr)
+    })
+  }
+
+  // Add event listener for the search button
+  searchBtn.on("click", function(e){
+    e.preventDefault();
+    searchCity()
 })
 
